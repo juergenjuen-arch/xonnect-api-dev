154c5f9bdf25c90db962c9f12330c8caf2a7abb1
JJU <office@jmp.at>
2026-04-30 19:12:03 +0200

    add prisma project endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
+import { ProjectsService } from './projects/projects.service';
+import { ProjectsController } from './projects/projects.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ProjectsController],
+  providers: [AppService, PrismaService, ProjectsService],
 })
 export class AppModule {}
